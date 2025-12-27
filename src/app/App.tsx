02bdcaec54ea359b45db8de45d7e541bac1f41ea
Anton Charadnik <anton.charadnik@modsen-software.com>
2025-12-27 03:08:21 +0300

    chore(*): create file base structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import { createSupabaseClient } from '@/shared/config/createSupabaseClient'
 import { Button } from '@/shared/ui/button'
 
-import { MainLayout } from './layouts/MainLayout'
+import { MainLayout } from '../shared/layouts/MainLayout'
 
 function App() {
   useEffect(() => {
